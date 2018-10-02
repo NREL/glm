5fc1d61d739c5e211e793035e62bac6991c16cba
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-10-02 10:38:26 -0600

    Update glm.nim

--- src/glm.nim
+++ src/glm.nim
@@ -3,16 +3,22 @@
 from utils import nil
 from logging as logger import nil
 import json
+import nimpy
 
 import ./parser
 import ./ast
 
-proc read_string(file: string): string =
-    let x = readFile(file)
-    return x
+proc loads(model: string, pretty: bool = false): string {. exportpy .} =
+    var p = initParser(model)
+    p.walk()
+    return $(p.ast.toJson())
+
+proc load(file: string, pretty: bool = false): string {. exportpy .} =
+    var model = read_file(file)
+    return loads(model)
 
 proc parse(file: string): AST =
-    var x = read_string(file)
+    var x = read_file(file)
     var p = initParser(x)
     p.walk()
     return p.ast
