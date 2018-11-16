82b5ee6f6777879ebec160f2349e141b3d4b1048
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-11-16 09:33:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 from setuptools import setup, Distribution
 import subprocess
 import shlex
+import os
+import shutil
+
+from sys import platform
 
 
+# Tested with wheel v0.29.0
 class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
 
     def has_ext_modules(self):
         return True
@@ -18,9 +24,34 @@ version = (
     .strip("v")
 )
 
+if os.getenv("BUILD") == "windows" or platform == "win32":
+    ext = "pyd"
+elif platform == "darwin":
+    ext = "so"
+elif platform == "linux" or platform == "linux2":
+    ext = "so"
+
+
+def setup_package():
+    if not os.path.exists("./glm"):
+        os.makedirs("./glm")
+    shutil.copy("./lib/glm.{}".format(ext), "./glm/")
+    with open("./glm/__init__.py", "w") as f:
+        f.write("""from .glm import *""".strip())
+    with open("./MANIFEST.in", "w") as f:
+        f.write("recursive-include ./glm/ glm.*")
+
+
+setup_package()
+
 setup(
     name="glm",
     version=version,
     description="glm Python library",
+    packages=["glm"],
+    scripts=["bin/glm2json", "bin/json2glm"],
+    author="Dheepak Krishnamurthy",
+    author_email="me@kdheepak.com",
+    url="https://github.com/NREL/glm",
     distclass=BinaryDistribution,
 )
