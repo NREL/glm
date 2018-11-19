13c4585d7e712fad07d892172499dc8324eed134
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-11-19 13:30:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, Distribution
+from setuptools import setup
 import subprocess
 import shlex
 import os
@@ -7,16 +7,25 @@ import shutil
 
 from sys import platform
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
-# Tested with wheel v0.29.0
-class BinaryDistribution(Distribution):
-    """Distribution which always forces a binary package with platform name"""
+    class bdist_wheel(_bdist_wheel):
 
-    def has_ext_modules(self):
-        return True
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
 
-    def is_pure(self):
-        return False
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            # We don't contain any python source
+            python, abi = "py2.py3", "none"
+            return python, abi, plat
+
+
+except ImportError:
+    bdist_wheel = None
 
 
 version = (
@@ -55,5 +64,5 @@ setup(
     author="Dheepak Krishnamurthy",
     author_email="me@kdheepak.com",
     url="https://github.com/NREL/glm",
-    distclass=BinaryDistribution,
+    cmdclass={"bdist_wheel": bdist_wheel},
 )
