139e6aa26e9d231b760f0aee4ef8db2f3ce24aa6
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-11-19 14:45:30 -0700

    Update .travis.yml and setup.py

--- setup.py
+++ setup.py
@@ -7,25 +7,29 @@ import shutil
 
 from sys import platform
 
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
-    class bdist_wheel(_bdist_wheel):
-
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            # Mark us as not a pure python package
-            self.root_is_pure = False
-
-        def get_tag(self):
-            python, abi, plat = _bdist_wheel.get_tag(self)
-            # We don't contain any python source
-            python, abi = "py2.py3", "none"
-            return python, abi, plat
+from setuptools.dist import Distribution
 
 
-except ImportError:
-    bdist_wheel = None
+class BinaryDistribution(Distribution):
+
+    def is_pure(self):
+        return False
+
+
+class bdist_wheel(_bdist_wheel):
+
+    def finalize_options(self):
+        _bdist_wheel.finalize_options(self)
+        # Mark us as not a pure python package
+        self.root_is_pure = False
+
+    def get_tag(self):
+        python, abi, plat = _bdist_wheel.get_tag(self)
+        # We don't contain any python source
+        python, abi = "py2.py3", "none"
+        return python, abi, plat
 
 
 version = (
@@ -64,5 +68,9 @@ setup(
     author="Dheepak Krishnamurthy",
     author_email="me@kdheepak.com",
     url="https://github.com/NREL/glm",
+    zip_safe=False,
     cmdclass={"bdist_wheel": bdist_wheel},
+    include_package_data=True,
+    distclass=BinaryDistribution,
+    package_data={"glm": ["*{}".format(ext)]},
 )
