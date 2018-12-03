00b9064dd3bb60602de6fef733a7cf9133b40175
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-12-03 16:59:03 -0700

    Use pyImport for dumps

--- src/glm.nim
+++ src/glm.nim
@@ -10,25 +10,24 @@ pyExportModuleName("_glm")
 import ./parser
 import ./ast
 
-proc loads(data: string): PyObject {. exportpy .} =
+proc loads(data: string): PyObject {. exportpy, noinline .} =
     var p = initParser(data)
     p.walk()
     return pyImport("json").loads($(p.ast.toJson()))
 
-proc load(file: string): PyObject {. exportpy .} =
+proc load(file: string): PyObject {. exportpy, noinline .} =
     var data = read_file(file)
     return loads(data)
 
-proc dumps(data: PyObject): string {. exportpy .} =
-    let jsdata = nimpy.toJson( data )
+proc dumps(data: PyObject): string {. exportpy, noinline .} =
+    let jsdata = pyImport("json").dumps( data ).toJson()
     jsdata.toAst().toGlm()
 
-proc dump(data: PyObject, file: string): int {. exportpy .} =
-    let jsdata = nimpy.toJson( data )
-    let glm = jsdata.toAst().toGlm()
+proc dump(data: PyObject, file: string): int {. exportpy, noinline .} =
+    let glm = dumps(data)
     writeFile(file, glm)
 
-proc version(): string {. exportpy .} =
+proc version(): string {. exportpy, noinline .} =
     const versionString = staticExec("git describe --tags HEAD")
     return versionString
 
